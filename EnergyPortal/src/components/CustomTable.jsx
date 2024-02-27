83cda76626360ea1bb303917bb0663a2ec3b3c9d
Syed <sfuaadukms@outlook.com>
2024-02-27 15:44:01 +0000

    extracted table into custom component providing collection as prop

---
+++ EnergyPortal/src/components/CustomTable.jsx
@@ -0,0 +1,50 @@
+import { Table, Container } from "react-bootstrap";
+import { useState } from "react";
+
+const CustomTable = ({ tableData }) => {
+  const [itemQuery, setitemQuery] = useState("");
+
+  const handleItemQuery = (e) => {
+    setitemQuery(e.target.value);
+  };
+
+  //make this uppercase
+
+  const tableHeaders = tableData.length > 0 ? Object.keys(tableData[0]) : [];
+
+  return (
+    <Container>
+      <Table responsive>
+        <thead>
+          <tr>
+            {tableHeaders.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((data, index) =>
+            data.name.toLowerCase().includes(itemQuery.toLowerCase()) ? (
+              <tr key={index}>
+                {tableHeaders.map((header, index) => (
+                  <td key={index}>{data[header]}</td>
+                  //give me the value for
+                  //this record associated with X header
+                ))}
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </Table>
+
+      <input
+        type="text"
+        placeholder="Search inverter"
+        value={itemQuery}
+        onChange={handleItemQuery}
+      />
+    </Container>
+  );
+};
+
+export default CustomTable;
--- EnergyPortal/src/pages/Information.jsx
+++ EnergyPortal/src/pages/Information.jsx
@@ -1,51 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import solarInverterData from "../utils/inverterData";
-import { Table, Container } from "react-bootstrap";
-import { useState } from "react";
+import CustomTable from "../components/CustomTable";
 
 const Information = () => {
-  const [inverterQuery, setinverterQuery] = useState("");
-  const inverterData = useLoaderData(); //gets the data returned from loader
+  const inverterData = useLoaderData();
 
-  const handleInverterQuery = (e) => {
-    setinverterQuery(e.target.value);
-  };
-
-  return (
-    <Container>
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Brand</th>
-            <th>Power</th>
-            <th>NominalPower</th>
-          </tr>
-        </thead>
-        <tbody>
-          {inverterData.map(
-            (inverter) =>
-              inverter.name
-                .toLowerCase()
-                .includes(inverterQuery.toLowerCase()) && (
-                <tr key={inverter.id}>
-                  <td>{inverter.name}</td>
-                  <td>{inverter.brand}</td>
-                  <td>{inverter.power}</td>
-                  <td>{inverter.nominalPower}</td>
-                </tr>
-              )
-          )}
-        </tbody>
-      </Table>
-      <input
-        type="text"
-        placeholder="Search inverter"
-        value={inverterQuery}
-        onChange={handleInverterQuery}
-      />
-    </Container>
-  );
+  return <CustomTable tableData={inverterData} />;
 };
 
 export default Information;

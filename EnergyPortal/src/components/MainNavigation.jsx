1ce6b44dabb88dfe448699ce7b6bcac1a5c19072
Syed <sfuaadukms@outlook.com>
2024-02-27 13:33:24 +0000

    Add navigation and update home page content

--- EnergyPortal/src/components/MainNavigation.jsx
+++ EnergyPortal/src/components/MainNavigation.jsx
@@ -7,7 +7,6 @@ const MainNavigation = () => {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand as={Link}>Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">
               Home
--- EnergyPortal/src/pages/HomePage.jsx
+++ EnergyPortal/src/pages/HomePage.jsx
@@ -1,5 +1,3 @@
-import Button from "react-bootstrap/Button";
-
 const HomePage = () => {
   //<Button className="btn btn-secondary">Hello</Button>
   return (
@@ -9,6 +7,19 @@ const HomePage = () => {
         <p>
           You can view statistics about the irradiance, inverters and modules
         </p>
+        <ul>
+          <h5>What needs to be added:</h5>
+          <li>Basic data retrieval ✅</li>
+          <li>Ability to login/logout basic state for now</li>
+          <li>Add inverter data from asp.net minimal API</li>
+          <li>Add CI/CD Github Actions</li>
+          <li>Add some unit tests</li>
+
+          <li>
+            Implement weather data for some plants, add auto fetch to get newest
+            weather data every 5 mins can mock
+          </li>
+        </ul>
       </article>
     </main>
   );
